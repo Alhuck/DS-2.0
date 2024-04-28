4f4d9cee356cf2abdaa62c666ab3bc2e96541ca4
Alhuck <alhucka@gmail.com>
2024-04-28 18:55:40 -0400

    climbing stairs - easy problem

---
+++ src/ClimbingStairs.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbingStairs {
+
+    //
+    // This pattern follows the Fibonacci series
+    //
+    //           _|
+    //         _| 5 n(4)
+    //       _| 3 n(3)
+    //     _| 2 n(2)
+    //    | 1 n(1)
+    //    1 n(0)
+    //
+    public static int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return climbStairsWithMemo(n, new HashMap<Integer, Integer>());
+    }
+
+    public static int climbStairsWithMemo(int n, Map<Integer, Integer> m) {
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (!m.containsKey(n)) {
+            m.put(n, climbStairsWithMemo(n - 1, m) + climbStairsWithMemo(n - 2, m));
+        }
+
+        return m.get(n);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(climbStairs(10));
+
+    }
+}
